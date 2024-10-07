8673ef2f8377123b4e4066e47541ba1b89689fee
Valeri Gladunik <valeri.gladunik@tptlive.ee>
2024-10-07 16:51:14 +0300

    Add item list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <script setup>
 import { ref } from 'vue';
 
-let message = ref('Hello Vue!');
-setTimeout(() => {
-    message.value ='Something else'
-}, 10000);
+let newItem = ref('');
+let items = ref(['piim','viin','sai','leib','õlu','banaan']);
+
+function add() {
+    if (newItem.value.trim() !== '') {
+   items.value.push(newItem.value);
+    }
+    newItem.value = '';
+}
+
 </script>
 
 <template>
-<h1>{{ message }}</h1>
+<div class="container mt-2">
+    <div class="field has-addons">
+  <div class="control is-expanded">
+    <input class="input" type="text" v-model="newItem" @keydown.enter="add">
+  </div>
+  <div class="control">
+    <button class="button is-info" @click="add">
+      Add
+    </button>
+  </div>
+</div>
+<div class="content">
+    <ul>
+        <li v-for="item in items">{{ item }}</li>
+    </ul>
+</div>
+    
+</div>
 </template>
