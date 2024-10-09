15e709f729a6361b4de758b480dbcd6ce5f78a69
Valeri Gladunik <valeri.gladunik@tptlive.ee>
2024-10-09 16:57:02 +0300

    button

---
+++ src/components/Btn.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed } from 'vue';
+let props = defineProps({
+    color:{
+    primary: 
+    String,
+    required:false,
+    validator(value, props) {
+        return [
+        'primary',
+        'link',
+        'info',
+        'success',
+        'warning',
+        'danger',
+        'white',
+        'light',
+        'dark',
+        'text',
+        'ghost',
+        ].includes(value);
+    }
+    },
+    light: {
+        type: Boolean,
+        required: false,
+    }
+});
+
+let classNames = computed(( ) => {
+    let names = [];
+    if(props.color){
+        names.push('is-' + props.type);
+    }
+    if(light){
+        names.push('is-light');
+    }
+    console.log(props);
+    return names.join('');
+})
+</script>
+
+<template>
+  
+    <button class="button" :class="'is-'+type">
+        <slot></slot>
+    </button>
+</template>
