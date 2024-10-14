81599d2213e2ddd99ea750a5bfb30e06b1b71fde
Valeri Gladunik <valeri.gladunik@tptlive.ee>
2024-10-14 14:53:09 +0300

    Buttons completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
-import Btn from './components/Btn.vue';
+
 </script>
 <template>
-    <div class="container mt-2">
-        <Btn color="primary" light>Button is cool</Btn>
-    </div>
+
 </template>
