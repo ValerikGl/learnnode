38afab45d99499a53f5ce7ef45fbfd77f560fedc
Valeri Gladunik <valeri.gladunik@tptlive.ee>
2024-10-23 14:51:56 +0300

    Prev&Next

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -3,13 +3,29 @@ import axios from 'axios';
 import { ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
 let characters = ref([]);
-axios.get('https://rickandmortyapi.com/api/character').then(response => {
+let info = ref({});
+getCharacters('https://rickandmortyapi.com/api/character');
+
+
+function getCharacters(url){
+    axios.get(url).then(response => {
     console.log(response);
     characters.value = response.data.results;
+    info.value = response.data.info;
 });
+}
+function next(){
+    getCharacters(info.value.next);
+}
+
+function prev(){
+    getCharacters(info.value.prev);
+}
 </script>
 <template>
 <div class="container">
+    <button class="button is-primary" @click="prev" :disabled="!info.prev">Prev</button>
+    <button class="button is-primary" @click="next" :disabled="!info.next">Next</button>
     <div class="columns is-multiline">
         <div class="column is-one-quarter" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
